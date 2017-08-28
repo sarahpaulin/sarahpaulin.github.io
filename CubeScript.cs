6fa92e800e645851f69e548188539c83560d5001
sarahpaulin <30540099+sarahpaulin@users.noreply.github.com>
2017-08-28 20:19:21 +1200

    Add files via upload

---
+++ CubeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+	public Vector3 spinSpeed = Vector3.zero;
+	//define axis of spin as y axis
+	Vector3 spinAxis = new Vector3(0, 1, 0);
+
+	// Use this for initialization
+	void Start () {
+		// setsize(2.0f);
+		//this.transform.position = new vector3(0, 5, 3);
+		spinSpeed = new Vector3 (Random.value, Random.value, Random.value);
+		spinAxis = Vector3.up;
+		spinAxis.x = (Random.value - Random.value)*.1f;
+
+
+	}
+
+	public void SetSize(float size)
+	{
+		this.transform.localScale = new Vector3(size, size, size);
+	}
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(spinSpeed);
+		this.transform.RotateAround (Vector3.zero, spinAxis, rotateSpeed);
+		// comment out above section when you dont want spin 
+		//cube is individual cube 
+
+		//Renderer rend = GetComponent<Renderer>();
+		//rend.material.shader = Shader.Find("specular");
+		//rend.material.color = new Color (Random.value, Random.value, Random.value);
+
+	}
+}
---
+++ sketch.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sketch : MonoBehaviour {
+
+	public GameObject myPrefab;
+
+	//public UnityEngine.GameObject[] cubes;
+	//public int cube = 0;
+
+
+	void Start () {
+		int totalCubes = 30;
+		float totalDistance = 2.9f;
+			float duration = 1.5f;
+
+
+		
+		//cubes = new GameObject[30];
+		//Linear distro
+		//for (int i = 0; i < totalCubes; i++) 
+		//{
+			//float perc = i / (float)totalCubes;
+			//float x = perc * totalDistance;
+			//float y = 3.0f; 
+			//float z = 0.0f;
+			//var newCube = (GameObject)Instantiate (myPrefab, new Vector3 (x, y, z), Quaternion.identity);
+			//newCube.GetComponent<CubeScript> ().SetSize (1.0f - perc);
+			//newCube.GetComponent<CubeScript> ().rotateSpeed = 0; // perc;// Random.value;
+		//}
+		// SIN() distro 
+		for (int i = 0; i < totalCubes; i++) 
+		{
+			//making cubes fit the screen by working out percentage 
+			float perc = i / (float)totalCubes;
+			//used for distribution of space between the cubes 
+			float sin = Mathf.Sin(perc * Mathf.PI/2);
+
+			//placing cubes equally distributed in the x axis 
+			float x = 1.8f + sin * totalDistance;
+			float y = 5.0f; 
+			float z = 0.0f;
+
+			//assigning the cibes to a random space 
+			//float x = (Random.value - Random.value)*3;
+			//float y = (Random.value)*8;
+			//float z = (Random.value - Random.value)*5;
+
+			//Have to deviate the initial x position from 0 to spin off center 
+			var newCube = (GameObject)Instantiate (myPrefab, new Vector3 (x, y, z), Quaternion.identity);
+			newCube.GetComponent<CubeScript> ().SetSize (.45f*(1.0f - perc));
+			newCube.GetComponent<CubeScript> ().rotateSpeed = .2f + perc*4.0f; // perc;// Random.value;
+		
+			//sketch is layout view and scene view 
+
+			//cubes[i] = newCube;
+		}
+		//InvokeRepeating ("ChangeColor", 2.0f, 1.0f);
+	}
+	
+
+	void Update () {
+		
+	}
+
+	//void colorChanger() {
+		//cubes[cube].GetComponent<Renderer>().material.color = Random.ColorHSV (0f, 1f, 1f, 1f, 0.5f, 1f);
+		//if (cube < 29) {
+			//cube = cube + 1;
+		//} 
+		//else {
+			//cube = 0;
+		//}
+}
+
